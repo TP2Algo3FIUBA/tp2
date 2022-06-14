b10bb4fc01aba0597d952ad2aa2f8289899ec686
Danikim01 <dakim@fi.uba.ar>
2022-06-14 12:03:45 -0300

    Implemente los test unitarios de Sorpresas favorables, desafavorables y de cambios de vehiculo.

--- src/main/test/edu/fiuba/algo3/modelo/testSorpresas.java
+++ src/main/test/edu/fiuba/algo3/modelo/testSorpresas.java
@@ -5,6 +5,8 @@ import edu.fiuba.algo3.modelo.jugador.Jugador;
 import edu.fiuba.algo3.modelo.manzana.Cuadra;
 import edu.fiuba.algo3.modelo.manzana.Esquina;
 import edu.fiuba.algo3.modelo.sorpresa.SorpresaCambioDeVehiculo;
+import edu.fiuba.algo3.modelo.sorpresa.SorpresaDesfavorable;
+import edu.fiuba.algo3.modelo.sorpresa.SorpresaFavorable;
 import edu.fiuba.algo3.modelo.vehiculo.Auto;
 import edu.fiuba.algo3.modelo.vehiculo.CuatroPorCuatro;
 import edu.fiuba.algo3.modelo.vehiculo.Moto;
@@ -15,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class testSorpresas {
     @Test
-    void testSorpresaCambioDeAuto() {
+    void unaMotoChocaContraUnaSorpresaDeCambioDeVehiculoYSeTransformaAUnAuto() {
         // Arrange
         Esquina esquina00 = new Esquina();  //   00 --#-- 01
         Esquina esquina01 = new Esquina();
@@ -28,16 +30,154 @@ public class testSorpresas {
         Jugador jugador = new Jugador(new Moto());
         jugador.spawnearVehiculoEn(esquina00);
 
-        // Act & Assert
         jugador.moverEnDireccion("Este");
         assertTrue(jugador.getVehiculo() instanceof Auto);
+    }
 
-        // Act & Assert
-        jugador.moverEnDireccion("Oeste");
+
+    @Test
+    void unAutoChocaContraUnaSorpresaDeCambioDeVehiculoYSeTransformaAUnCuatroPorCuatro() {
+        // Arrange
+        Esquina esquina00 = new Esquina();  //   00 --#-- 01
+        Esquina esquina01 = new Esquina();
+
+        // conecto 00 con 01
+        Cuadra cuadra00_01 = new Cuadra(esquina00, esquina01, new SorpresaCambioDeVehiculo());
+        esquina00.insertarCuadra("Este", cuadra00_01);
+        esquina01.insertarCuadra("Oeste", cuadra00_01);
+
+        Jugador jugador = new Jugador(new Auto());
+        jugador.spawnearVehiculoEn(esquina00);
+
+        jugador.moverEnDireccion("Este");
         assertTrue(jugador.getVehiculo() instanceof CuatroPorCuatro);
+    }
 
-        // Act & Assert
+    @Test
+    void unCuatroPorCuatroChocaContraUnaSorpresaDeCambioDeVehiculoYSeTransformaAUnaMoto() {
+        // Arrange
+        Esquina esquina00 = new Esquina();  //   00 --#-- 01
+        Esquina esquina01 = new Esquina();
+
+        // conecto 00 con 01
+        Cuadra cuadra00_01 = new Cuadra(esquina00, esquina01, new SorpresaCambioDeVehiculo());
+        esquina00.insertarCuadra("Este", cuadra00_01);
+        esquina01.insertarCuadra("Oeste", cuadra00_01);
+
+        Jugador jugador = new Jugador(new CuatroPorCuatro());
+        jugador.spawnearVehiculoEn(esquina00);
+
         jugador.moverEnDireccion("Este");
         assertTrue(jugador.getVehiculo() instanceof Moto);
     }
+
+    @Test
+    void unaMotoChocaContraUnaSorpresaDesfavorableYLeSumaUn25PorcientoDeLosMovimientosHechos(){
+        Esquina esquina00 = new Esquina();  //   00 --#-- 01
+        Esquina esquina01 = new Esquina();
+
+        // conecto 00 con 01
+        Cuadra cuadra00_01 = new Cuadra(esquina00, esquina01, new SorpresaDesfavorable());
+        esquina00.insertarCuadra("Este", cuadra00_01);
+        esquina01.insertarCuadra("Oeste", cuadra00_01);
+
+        Jugador jugador = new Jugador(new Moto());
+        jugador.spawnearVehiculoEn(esquina00);
+
+        jugador.moverEnDireccion("Este");
+        jugador.moverEnDireccion("Oeste");
+        assertEquals(3,jugador.getMovimientos());
+    }
+
+    @Test
+    void unAutoChocaContraUnaSorpresaDesfavorableYLeSumaUn25PorcientoDeLosMovimientosHechos(){
+        Esquina esquina00 = new Esquina();  //   00 --#-- 01
+        Esquina esquina01 = new Esquina();
+
+        // conecto 00 con 01
+        Cuadra cuadra00_01 = new Cuadra(esquina00, esquina01, new SorpresaDesfavorable());
+        esquina00.insertarCuadra("Este", cuadra00_01);
+        esquina01.insertarCuadra("Oeste", cuadra00_01);
+
+        Jugador jugador = new Jugador(new Auto());
+        jugador.spawnearVehiculoEn(esquina00);
+
+        jugador.moverEnDireccion("Este");
+        jugador.moverEnDireccion("Oeste");
+        assertEquals(3,jugador.getMovimientos());
+    }
+
+    @Test
+    void unCuatroPorCuatroChocaContraUnaSorpresaDesfavorableYLeSumaUn25PorcientoDeLosMovimientosHechos(){
+        Esquina esquina00 = new Esquina();  //   00 --#-- 01
+        Esquina esquina01 = new Esquina();
+
+        // conecto 00 con 01
+        Cuadra cuadra00_01 = new Cuadra(esquina00, esquina01, new SorpresaDesfavorable());
+        esquina00.insertarCuadra("Este", cuadra00_01);
+        esquina01.insertarCuadra("Oeste", cuadra00_01);
+
+        Jugador jugador = new Jugador(new CuatroPorCuatro());
+        jugador.spawnearVehiculoEn(esquina00);
+
+        jugador.moverEnDireccion("Este");
+        jugador.moverEnDireccion("Oeste");
+        assertEquals(3,jugador.getMovimientos());
+    }
+
+    @Test
+    void unaMotoChocaContraUnaSorpresaFavorableYLeRestaUn25PorcientoDeLosMovimientosHechos(){
+        Esquina esquina00 = new Esquina();  //   00 --#-- 01
+        Esquina esquina01 = new Esquina();
+
+        // conecto 00 con 01
+        Cuadra cuadra00_01 = new Cuadra(esquina00, esquina01, new SorpresaFavorable());
+        esquina00.insertarCuadra("Este", cuadra00_01);
+        esquina01.insertarCuadra("Oeste", cuadra00_01);
+
+        Jugador jugador = new Jugador(new Moto());
+        jugador.spawnearVehiculoEn(esquina00);
+
+        jugador.moverEnDireccion("Este");
+        jugador.moverEnDireccion("Oeste");
+        assertEquals(0,jugador.getMovimientos());
+    }
+
+    @Test
+    void unAutoChocaContraUnaSorpresaFavorableYLeRestaUn25PorcientoDeLosMovimientosHechos(){
+        Esquina esquina00 = new Esquina();  //   00 --#-- 01
+        Esquina esquina01 = new Esquina();
+
+        // conecto 00 con 01
+        Cuadra cuadra00_01 = new Cuadra(esquina00, esquina01, new SorpresaFavorable());
+        esquina00.insertarCuadra("Este", cuadra00_01);
+        esquina01.insertarCuadra("Oeste", cuadra00_01);
+
+        Jugador jugador = new Jugador(new Auto());
+        jugador.spawnearVehiculoEn(esquina00);
+
+        jugador.moverEnDireccion("Este");
+        jugador.moverEnDireccion("Oeste");
+        assertEquals(0,jugador.getMovimientos());
+    }
+
+    @Test
+    void unCuatroPorCuatroChocaContraUnaSorpresaFavorableYLeRestaUn25PorcientoDeLosMovimientosHechos(){
+        Esquina esquina00 = new Esquina();  //   00 --#-- 01
+        Esquina esquina01 = new Esquina();
+
+        // conecto 00 con 01
+        Cuadra cuadra00_01 = new Cuadra(esquina00, esquina01, new SorpresaFavorable());
+        esquina00.insertarCuadra("Este", cuadra00_01);
+        esquina01.insertarCuadra("Oeste", cuadra00_01);
+
+        Jugador jugador = new Jugador(new CuatroPorCuatro());
+        jugador.spawnearVehiculoEn(esquina00);
+
+        jugador.moverEnDireccion("Este");
+        jugador.moverEnDireccion("Oeste");
+        assertEquals(0,jugador.getMovimientos());
+    }
+
+
 }
